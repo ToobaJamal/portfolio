745dce1c7ca6aa5249de927f983f3ef0de5571bc
Tooba <jtooba70@gmail.com>
2023-01-06 20:08:14 +0500

    Create Hire page

---
+++ src/pages/Hire.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function Hire() {
+    return (
+        <div>
+            <h1>Hi, I am Tooba</h1>
+            <h1>I am a freelance front-end web developer and technical writer</h1>
+            
+        </div>
+    )
+}
