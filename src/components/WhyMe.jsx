fd899a325fdc81ff2e9eda8270c8db74047b84b4
Tooba <jtooba70@gmail.com>
2023-01-06 20:07:24 +0500

    Create WhyMe component

---
+++ src/components/WhyMe.jsx
@@ -0,0 +1,12 @@
+export default function WhyMe() {
+    return (
+        <>
+            <h1>Why hire me?</h1>
+            <div>
+                <p>Web Development</p>
+                <p>Technical Writing</p>
+                <p>Code Refactoring</p>
+            </div>
+        </>
+    )
+}
