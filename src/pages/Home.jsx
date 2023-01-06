180831943995f69eec5e63fad689910aaa099afa
Tooba <jtooba70@gmail.com>
2023-01-06 20:04:53 +0500

    Edit Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom"
+import AboutMeShort from "../components/AboutMeShort"
+import WhyMe from "../components/WhyMe"
+import Testimonials from "../components/Testimonials"
 
 export default function Home() {
     return (
         <div className="home">
             <h1>Hi, I am Tooba</h1>
-            <h1>I am a freelance software developer specialized in front-end web development and I love what I do</h1>
+            <h1>I am a freelance front-end web developer and technical writer</h1>
+            <AboutMeShort />
+            <WhyMe />
+            <Testimonials />
         </div>
     )
 }
