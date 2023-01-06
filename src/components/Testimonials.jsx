8de2c3260fa6b3b957373a0a6cf78f0f8bf56ec1
Tooba <jtooba70@gmail.com>
2023-01-06 20:06:59 +0500

    Create Testimonials component

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+export default function Testimonials() {
+    return (
+        <div className="testimonials">
+           <div>
+            <h3>Winifred John</h3>
+           </div>
+           <div>
+            <h3>Josh Weinstein</h3>
+           </div>
+           <div>
+            <h3>Deeti Singh</h3>
+           </div>
+        </div>
+    )
+}
