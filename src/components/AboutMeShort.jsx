a8568ec542ac94223011223cffbc41430aa33d67
Tooba <jtooba70@gmail.com>
2023-01-06 20:05:32 +0500

    Create AboutMeShort component

---
+++ src/components/AboutMeShort.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export default function AboutMeShort() {
+    return (
+        <>
+            <h1>About Me</h1>
+            <p>My name is Tooba. I build websites and break complex technical information into easy to understand sentences.</p>
+        </>
+    )
+}
