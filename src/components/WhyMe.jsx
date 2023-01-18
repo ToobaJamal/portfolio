145248462493ad261413188153a501748db3d885
Tooba <jtooba70@gmail.com>
2023-01-18 19:11:01 +0500

    Edit WhyMe component

--- src/components/WhyMe.jsx
+++ src/components/WhyMe.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-
+import { Title } from '../pages/About'
 const WhyMeSection = styled.div`
     height: 100vh;
     text-align: center;
@@ -7,18 +7,47 @@ const WhyMeSection = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    width: 100vw;
+    padding-block: 1em;
 `
 
-const Title = styled.h1`
-    color: red;
-`
-
-const Values = styled.div`
-   border: 1px solid yellow;
+// const Title = styled.h1`
+//     margin-bottom: 0.8em;
+// `
+const ServiceContainer = styled.div`
+    width: 400px;
 `
 
 const Services = styled.div`
-   border: 1px solid yellow;
+   display: flex;
+   flex-flow: row wrap;
+   align-items: center;
+   justify-content: center;
+   gap: 1em;
+   padding: 1em;
+   max-width: 1000px;
+   min-width: 0;
+   margin-top: 2em;
+   @media (max-width: 800px) {
+    width: 90%;
+    gap: 0.6em;
+   }
+`
+
+const ServiceTitle = styled.p`
+    font-size: 1.3em;
+    font-weight: 600;
+    margin: 0;
+`
+
+const ServiceDesc = styled.p`
+    font-size: 1em;
+    font-weight: 500;
+    margin: 0;
+    padding: 1em;
+    @media (max-width: 400px) {
+        
+       }
 `
 
 export default function WhyMe() {
@@ -26,14 +55,19 @@ export default function WhyMe() {
         <>
             <WhyMeSection>
                 <Title>Why hire me?</Title>
-                <Values>
-                    <p>Collaboration</p>
-                    <p>Quality</p>
-                </Values>
                 <Services>
-                    <p>Web Development</p>
-                    <p>Technical Writing</p>
-                    <p>Code Refactoring</p>
+                    <ServiceContainer>
+                        <ServiceTitle>Web Development</ServiceTitle>
+                        <ServiceDesc>Turn your idea into a user-friendly website.</ServiceDesc>
+                    </ServiceContainer>
+                    <ServiceContainer>
+                        <ServiceTitle>Technical Writing</ServiceTitle>
+                        <ServiceDesc>Make complicated topics easy for audience to understand for your blog/ business</ServiceDesc>
+                    </ServiceContainer>
+                    <ServiceContainer>
+                        <ServiceTitle>Code Refactoring</ServiceTitle>
+                        <ServiceDesc>Refactor your ugly lookin website into a modern one or kill bugs in your code</ServiceDesc>
+                    </ServiceContainer>
                 </Services>
             </WhyMeSection>
         </>
