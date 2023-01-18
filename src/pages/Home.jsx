65908668c0416acd7295cde1d712c026a9094f10
Tooba <jtooba70@gmail.com>
2023-01-18 19:12:52 +0500

    Edit Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,85 @@
-import AboutMeShort from "../components/AboutMeShort"
 import WhyMe from "../components/WhyMe"
 import Testimonials from "../components/Testimonials"
-import styled from 'styled-components'
+import Portfolio from "../components/Portfolio"
+import styled, {keyframes} from 'styled-components'
+
+export const anim = keyframes`
+0% {
+    transform: translateX(-100%);
+}
+100% {
+    transform: translateX(0);
+}
+`
 
 const IntroSection = styled.div`
-    height: 100vh;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+height: 100vh;
+text-align: center;
+display: flex;
+flex-direction: column;
+justify-content: center;
+
+width: 50vw;
+padding-block: 1em;
+padding-inline: 2em;
+margin-inline: auto;
+
+@media (max-width: 802px) {
+    width: 70%;
+}
+@media (max-width: 600px) {
+    width: 90%;
+}
+`
+const IntroTitleText = styled.p`  
+    margin: 0;
+    font-size: 1.3em;
+    font-weight: 500;
+    width: 100%;
+    text-align: left;
+
+    @media(max-width: 440px) {
+        font-size: 1.3em;
+    }
 `
 
 const IntroTitle = styled.h1`
-    text-decoration-line: underline;
-    
+    color: #DE3163;
+    text-align: left;
+    margin: 0; 
+    padding: 0;
+
+    @media(max-width: 500px) {
+        font-size: 3em;
+    }
+    @media(max-width: 440px) {
+        font-size: 2em;
+    }
 `
 
 const IntroDesc = styled.p`
-    text-decoration-line: underline;
+    margin: 0;
+    font-size: 1.3em;
+    font-weight: 500;
+    width: 100%;
+    text-align: left;
+
+    @media(max-width: 440px) {
+        font-size: 1em;
+    }
 `
 export default function Home() {
+    
     return (
         <>
             <IntroSection>
-                <IntroTitle>Hi, I am Tooba</IntroTitle>
-                <IntroDesc>I am a freelance front-end web developer and technical writer</IntroDesc>
+                <IntroTitleText>Hi, My name is</IntroTitleText>
+                <IntroTitle>Tooba Jamal</IntroTitle>
+                <IntroDesc>I am a front-end web developer and a technical writer</IntroDesc>
             </IntroSection>
-            <AboutMeShort />
-            <WhyMe />
-            <Testimonials />
+            <WhyMe/>
+            <Portfolio/>
+            <Testimonials/>
         </>
     )
 }
