781f6d90dfaaff744e308f3db7ebc727a90dc8d0
Tooba <jtooba70@gmail.com>
2023-01-28 12:20:07 +0500

    Remove target blank

--- src/pages/Hire.jsx
+++ src/pages/Hire.jsx
@@ -9,7 +9,7 @@ export default function Hire() {
             <Title>Web Developer and Technical Writer</Title>
             <Desc>I'm a web developer and technical writer with a background in machine learning. 
                 I, specialize in <Highlight>front-end development</Highlight> with strong <Highlight>JavaScript</Highlight> and 
-                <Highlight>React</Highlight> skills. 
+                <Highlight> React</Highlight> skills. 
                 I also love writing and combined my writing skills in tech I became a <Highlight>technical writer</Highlight>.
                 I'm also an experienced machine learning engineer who has worked on projects in Python, 
                 Jupyter Notebooks, and Git. My toolkit includes <Highlight>HTML, CSS, JavaScript, React, Machine Learning, 
@@ -19,13 +19,13 @@ export default function Hire() {
                 I'll help make sure it's cleanly written so everyone can understand it easily.
                 I love what I do, and building websites that provide better user experience is my passion. 
                 I've written technical articles for famous publications including <Highlight>FreeCodeCamp</Highlight> and 
-                <Highlight>Towards Data Science</Highlight>. 
+                <Highlight> Towards Data Science</Highlight>. 
                 My content is known for being easy to understand no matter how complicated the topic is.
                 Currently looking for life-changing opportunities. Get in touch if you have got any!</Desc>
                 <SocialIconContainer>
-            <a href='https://www.linkedin.com/in/tooba-jamal/' target="_blank"><SocialIcon src='./images/linkedin.png'/></a>
-            <a href='https://github.com/ToobaJamal' target="_blank"><SocialIcon src='./images/github.png'/></a>
-            <a href='https://www.linkedin.com/in/tooba-jamal/' target="_blank"><SocialIcon src='./images/insta.png'/></a>
+            <a href='https://www.linkedin.com/in/tooba-jamal/'><SocialIcon src='./images/linkedin.png'/></a>
+            <a href='https://github.com/ToobaJamal'><SocialIcon src='./images/github.png'/></a>
+            <a href='https://www.linkedin.com/in/tooba-jamal/'><SocialIcon src='./images/insta.png'/></a>
             </SocialIconContainer>
             </ContainerDiv>
         </>
